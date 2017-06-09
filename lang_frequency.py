d6066b2557b4e4a045c7fbf98911e714315cfb1e
antauren <antauren@yandex.ru>
2017-06-09 07:00:08 +0300

    исправлено:
    "Вместо табов для отступов стоит использовать пробелы. Избавься от табов и настрой редактор."

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,43 +5,43 @@ import operator
 
 
 def load_data(filepath):
-	f = open ( filepath , 'r', encoding='windows-1251' )
-	return f
-	f.close()
+    f = open(filepath, 'r', encoding='windows-1251')
+    return f
+    f.close()
 
 
 def get_10_most_frequent_words(text):
-	dict_word_frequency = {}
-	pattern = r'[а-я]|[a-z]+'
+    dict_word_frequency = {}
+    pattern = r'[а-я]|[a-z]+'
 
-	for line in text:
-		words = re.findall(pattern, line, re.IGNORECASE)
-		for word in words:
-			word = word.lower()
-			if word in dict_word_frequency:
-				dict_word_frequency[word] += 1
-			else:
-				dict_word_frequency[word] = 1
+    for line in text:
+        words = re.findall(pattern, line, re.IGNORECASE)
+        for word in words:
+            word = word.lower()
+            if word in dict_word_frequency:
+                dict_word_frequency[word] += 1
+            else:
+                dict_word_frequency[word] = 1
 
-	sorted_dict = sorted(dict_word_frequency.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_dict = sorted(dict_word_frequency.items(), key=operator.itemgetter(1), reverse=True)
 
-	counter = 0
-	print('\nслово и количество его упоминаний:\n')
-	for record in sorted_dict:
-		word = record[0]
-		count_of_word = record[1]
+    counter = 0
+    print('\nслово и количество его упоминаний:\n')
+    for record in sorted_dict:
+        word = record[0]
+        count_of_word = record[1]
 
-		print(word, count_of_word)
-		counter += 1
-		if counter >= 10:
-		 	break
+        print(word, count_of_word)
+        counter += 1
+        if counter >= 10:
+            break
 
 
 def start_script(argv_list):
     if len(argv_list) < 2:
-    	print('Вас привествует программа подсчета слов в тексте\n'
-    		'Для того чтоб узнать как работает программа введите: python lang_frequency.py --help или python3 lang_frequency.py --help')
-    	return None
+        print('Вас привествует программа подсчета слов в тексте\n'
+              'Для того чтоб узнать как работает программа введите: python lang_frequency.py --help или python3 lang_frequency.py --help')
+        return None
 
     if argv_list[1] == '--help':
         print('\nЧтобы запустить программу, нужно ввести в консоли: <интерпретатор> <скрипт> <текстовый файл>\n'
@@ -60,5 +60,5 @@ def start_script(argv_list):
 
 
 if __name__ == '__main__':
-	argv_list = sys.argv
-	start_script(argv_list)
+    argv_list = sys.argv
+    start_script(argv_list)
