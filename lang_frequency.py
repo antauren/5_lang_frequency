e4be3a0a4a2ab8909ccde018e3cb6b8a775ddee6
alex t <antauren@yandex.ru>
2017-07-09 03:34:10 +0300

    исправлено следующее:
    -Одна функция должна делать что-то одно: преобразовывать данные, вводить или выводить. Научись отделять обработку данных от ввода/вывода и стремись создавать функции чистыми. (output и считает слова и выводит их)
    
    -Попробуй избавиться от try...except в ifmain. Это уменьшит вложенность и упростит код.

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,24 +3,16 @@ import os.path
 import re
 from collections import Counter
 
-def load_data(filepath):
-    with open(filepath, 'r', encoding='windows-1251') as file:
-        return file.read()
 
-def get_most_frequent_words(text):
-    pattern = r'[а-я]|[a-z]+'
-    words = re.findall(pattern, text.lower())
-    return Counter(words)
+def print_help():
+    print(
+        'Для того чтоб узнать как работает программа введите: python lang_frequency.py --help или python3 lang_frequency.py --help')
 
-def output(most_frequent_words, quantity=10):
-    print('\nслово и количество его упоминаний:\n')
-    for record in most_frequent_words.most_common(quantity):
-        word, count_of_word = record
-        print(word, count_of_word)
 
 def get_filepath(argv_list):
     if len(argv_list) < 2:
         print('Вас привествует программа подсчета слов в тексте\n')
+        print_help()
         return None
 
     if argv_list[1] == '--help':
@@ -32,17 +24,43 @@ def get_filepath(argv_list):
 
     if not os.path.exists(filepath):
         print('Ошибка: программа не может найти текстовый файл\n')
+        print_help()
         return None
     else:
         return filepath
 
 
-if __name__ == '__main__':
+def load_data(filepath):
+    with open(filepath, 'r', encoding='windows-1251') as file:
+        return file.read()
+
+
+def get_most_frequent_words(text):
+    pattern = r'[а-я]|[a-z]+'
+    words = re.findall(pattern, text.lower())
+    return Counter(words)
+
+
+def word_count(most_frequent_words, quantity=10):
+    return most_frequent_words.most_common(quantity)
+
+
+def output():
     argv_list = sys.argv
     filepath = get_filepath(argv_list)
-    try:
-        text = load_data(filepath)
-        most_frequent_words = get_most_frequent_words(text)
-        output(most_frequent_words)    
-    except TypeError:
-        print('Для того чтоб узнать как работает программа введите: python lang_frequency.py --help или python3 lang_frequency.py --help')
+
+    if filepath is None:
+        return None
+
+    text = load_data(filepath)
+    most_frequent_words = get_most_frequent_words(text)
+    records = word_count(most_frequent_words)
+
+    print('\nслово и количество его упоминаний:\n')
+    for record in records:
+        word, count_of_word = record
+        print(word, count_of_word)
+
+
+if __name__ == '__main__':
+    output()
